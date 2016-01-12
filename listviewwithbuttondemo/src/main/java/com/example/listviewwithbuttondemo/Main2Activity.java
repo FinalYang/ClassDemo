75bc7f3e0ceacf14425e8dd1c3e650b8b1e908e6
chenjunbo <5433708@qq.com>
2016-01-12 09:23:40 +0800

    listview中包含 button 和 checkbox 的情况常见问题解决

--- listviewwithbuttondemo/src/main/java/com/example/listviewwithbuttondemo/Main2Activity.java
+++ listviewwithbuttondemo/src/main/java/com/example/listviewwithbuttondemo/Main2Activity.java
@@ -1,7 +1,7 @@
 package com.example.listviewwithbuttondemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class Main2Activity extends AppCompatActivity {
     private Aaaaaaaaaaaaa aaaaaaaaaaaaa;
